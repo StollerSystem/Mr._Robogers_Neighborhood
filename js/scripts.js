2fcab74b2321f721dd139419a67ca0963e720d89
Ben Stoller <stollersystemsound@gmail.com>
2020-08-21 09:07:38 -0700

    build out function for spec 2, not working yet

--- js/scripts.js
+++ js/scripts.js
@@ -10,6 +10,29 @@ function returnRange(input){
   }
   return range
 }
+
+function numberCheck(input){
+  // if (input.split(" ").includes(/\d/g)) {
+  //   console.log("IS NUMBERS!");
+  // } else {
+  //   console.log("NOT NUMBERS!");
+  // }
+
+  if (input.split(" ").length > 1) {
+    alert("PLease enter a SINGLE number")
+   } else if (parseInt(input) === NaN) {
+     alert("Please enter a NUMBER!")
+   } else {
+     console.log("ALL GOOD")
+   }
+
+
+}
+
+
+
+//console.log(parseInt("test"))
+console.log(numberCheck("fuck"))
 console.log(returnRange(20))
 
 
